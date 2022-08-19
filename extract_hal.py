5e1fe8b88e26cef05cc678b4a7b3a4b04cf0ed11
Mathieu Léonardon <mathieu.leonardon@imt-atlantique.fr>
2022-08-19 11:18:36 +0200

    Generates files for website.

--- extract_hal.py
+++ extract_hal.py
@@ -1,4 +1,9 @@
+from urllib import response
 import requests
+import json
+import os
+import shutil
+
 
 def afficher_erreur_api(erreur):
     """Affiche les erreurs soulevées lors de l'interrogation de l'API HAL
@@ -41,4 +46,69 @@ if __name__ == '__main__':
     for name in dictionary:
         with open(name + '.bib', 'w') as file:
             file.write(dictionary[name])
+
+fields="fl=abstract_s,docid,label_s,title_s, producedDate_tdate,uri_s,"
+fields+="journalTitle_s,conferenceTitle_s,journalPublisher_s,doiId_s,docType_s,"
+fields+="authLastName_s,producedDateY_i,authFullName_s,keyword_s,fileMain_s"
+
+requete_json = api_prefix +"&wt=json&"+fields+"&sort=producedDateY_i desc"
+
+
+if __name__ == '__main__':
+    try:
+        reponse = requests.get(requete_json, timeout=5).json()
+
+    except requests.exceptions.HTTPError as errh:
+        afficher_erreur_api(errh)
+    except requests.exceptions.ConnectionError as errc:
+        afficher_erreur_api(errc)
+    except requests.exceptions.Timeout as errt:
+        afficher_erreur_api(errt)
+    except requests.exceptions.RequestException as err:
+        afficher_erreur_api(err)
+    
+
+    # dump jsons in files
+    with open('full.json', 'w') as file:
+        json.dump(reponse, file, indent=2, ensure_ascii=False)
+
+    path = './website/publication/'
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    os.makedirs(path)
+
+    pubtype_dict = {"THESE" : "7", "ART" : "2", "COMM" : "1"}
+
+    for pub in reponse['response']['docs']:
+        if pub['docType_s'] not in pubtype_dict.keys():
+            continue
+        dirname = str(pub['producedDateY_i'])
+        dirname += '-'
+        dirname += ''.join(e for e in pub['authLastName_s'][0] if e.isalnum())
+        dirname += '-'
+        dirname += pub['docid']
+        dirname += '/'
+        os.makedirs(path+dirname)
+        with open(path+dirname+'index.md', 'w') as f:
+            f.write('+++\n')
+            f.write('title = \"' + pub['title_s'][0]+ '\"\n')
+            f.write('date = ' + pub['producedDate_tdate']+ '\n')
+            f.write('authors = ' + str(pub['authFullName_s'])+ '\n')
+            f.write('publication_types = [\"' + pubtype_dict[pub['docType_s']]+ '\"]\n')
+            f.write('abstract = \"' + pub['abstract_s'][0].replace('\"','\'')+ '\"\n')
+            f.write('featured = false\n')
+            if pub['docType_s'] == "ART":
+                f.write('publication = \"' + pub['journalTitle_s'] + ' (' + pub['journalPublisher_s'] +')\"\n')
+            elif pub['docType_s'] == "COMM":
+                f.write('publication = \"' + pub['conferenceTitle_s'] + '\"\n')
+            if 'keyword_s' in pub.keys():
+                f.write('tags = ' + str(pub['keyword_s']) + '\n')
+            if 'doiId_s' in pub.keys():
+                f.write('doi = \"' + pub['doiId_s'] + '\"\n')
+            if 'fileMain_s' in pub.keys():
+                f.write('url_pdf = \"' + pub['fileMain_s'] + "\"\n")
+            f.write('+++\n')
+
+
+
 
